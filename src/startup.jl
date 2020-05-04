b2b83a0961d1f98ad7e47cdd2af99ee1131803c8
Leo Huang <albert11813@gmail.com>
2020-05-04 18:19:08 -0400

    startup file

---
+++ src/startup.jl
@@ -0,0 +1,24 @@
+using Pkg
+Pkg.add("ArgParse")
+Pkg.add("Clustering")
+Pkg.add("MLDatasets")
+Pkg.add("Statistics")
+Pkg.add("Distances")
+Pkg.add("Distributions")
+Pkg.add("ProgressMeter")
+Pkg.add("Dates")
+Pkg.add("Printf")
+using LinearAlgebra
+using Plots
+using MLDatasets
+using Statistics
+using Random
+using Distances
+using Distributions
+using Roots
+using ArgParse
+using Clustering
+using Combinatorics
+using ProgressMeter
+using Dates
+using Printf
