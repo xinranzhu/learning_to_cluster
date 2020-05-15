0308212f5ec87aece19ca0ef4f4ab104e0453204
Leo Huang <albert11813@gmail.com>
2020-05-15 04:50:14 -0400

    normalized attribute matrix

--- src/attributed/clusteringAttributed.jl
+++ src/attributed/clusteringAttributed.jl
@@ -1,5 +1,35 @@
 include("readAttributed.jl")
 
+"""
+Normalize the edge attributes of A (stored in P) to be in [0, 1]
+"""
+function normalizeAttributes(P)
+    Pn = similar(P)
+    maxs = zeros(11)
+    rows = rowvals(P)
+    vals = nonzeros(P)
+    m, n = size(P)
+    for j = 1:n
+        for i in nzrange(P, j)
+            val = vals[i]
+            for k = 1:length(val)
+                if val[k] > maxs[k]
+                    maxs[k] = val[k]
+                end
+            end
+        end
+    end
+    for j = 1:n
+        for i in nzrange(P, j)
+            val = vals[i]
+            row = rows[i]
+            Pn[row, j] = P[row, j] ./ maxs
+        end
+    end
+    return Pn
+end
+
+
 function removeWeights(A)
     rows = rowvals(A)
     vals = nonzeros(A)
@@ -23,7 +53,9 @@ end
 A = getBodyWeightedAdj()   #weighted adjacency matrix
 B = removeWeights(A) #unweighted adj matrix
 P = getBodyAttributeAdj()  #adjacency matrix of attributes (recall that graph is edge-attributed)
+Pn = normalize(P)
 D =  dropdims(sum(A, dims = 2), dims=2) #Degree matrix
+
 
 
 
